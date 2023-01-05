bfcb31d06e4c59dc82cfccf4e7d3a93216c9d4d7
Carlos Bermeo <carlos.xbr54@gmail.com>
2023-01-05 13:53:01 -0500

    First commit
    Adding all the files to github

---
+++ Controllers/TiendaController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TiendaController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Query()
+        {
+            TestContext db = new TestContext();
+
+            var data = db.Database.SqlQuery<Tienda>("spTiendaQuery").ToList();
+
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int IdTienda)
+        {
+            TestContext db = new TestContext();
+
+            var data = db.Database.SqlQuery<Tienda>("spTiendaQuery @IdTienda = {0}", IdTienda).FirstOrDefault();
+
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Insert(Tienda model)
+        {
+            TestContext db = new TestContext();
+
+            db.Database.ExecuteSqlCommand("spTiendaInsert @Nombre = {0}, @Direccion = {1}, @Telefono = {2}, @Latitud = {3}, @Longitud = {4}", 
+            model.Nombre, model.Direccion, model.Telefono, model.Latitud, model.Longitud);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Update(Tienda model)
+        {
+            TestContext db = new TestContext();
+
+            db.Database.ExecuteSqlCommand("spTiendaUpdate @IdTienda = {0}, @Nombre = {1}, @Direccion = {2}, @Telefono = {3}, @Latitud = {4}. @Longitud = {5}",
+              model.IdTienda, model.Nombre, model.Direccion, model.Telefono, model.Latitud, model.Longitud);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Delete(Tienda model)
+        {
+            TestContext db = new TestContext();
+
+            db.Database.ExecuteSqlCommand("spTiendaDelete @IdTienda = {0}", model.IdTienda);
+
+            return Ok();
+        }
+    }
+}
---
+++ Models/Tienda.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Tienda
+    {
+        public int IdTienda { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+    }
+}
